5a74212489d70ca34f96f8b634ae42a4de91488f
DaniloFaustino <danilovid@yahoo.com.br>
2017-05-03 21:44:27 -0300

    Tentativa de fazer o exercício 2

--- Soluções/ExerciciosRevisao/ExerciciosRevisao/Program.cs
+++ Soluções/ExerciciosRevisao/ExerciciosRevisao/Program.cs
@@ -12,30 +12,37 @@ namespace ExerciciosRevisao
         {
             string NomeModelo, ClassifQuil = "", ClassifPot = "";
             double quilometro, potencia;
+            int N, i;
 
-            Console.WriteLine("Informe o modelo do veículo");
-            NomeModelo = Console.ReadLine();
-            Console.WriteLine("Informe a quantidade de quilômetros rodados");
-            quilometro = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Informe a potência do motor");
-            potencia = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Indique o número de veículos");
+            N = Convert.ToInt32(Console.ReadLine());
 
-            if (quilometro <= 5000)
-                ClassifQuil = "Novo";
-            else if (quilometro > 5000 && quilometro <= 30000)
-                ClassifQuil = "Seminovo";
-            else 
-                ClassifQuil = "Velho";
-           
-            if (potencia > 200)
-                ClassifPot = "Potente";
-            else if (potencia == 120 && potencia <= 200)
-                ClassifPot = "Forte";
-            else 
-                ClassifPot = "Popular";
+            for (i = 0, i < N, i++ )
+            {
 
-            Console.WriteLine("{0} - {1} - {2}", NomeModelo, ClassifQuil, ClassifPot);
-            
+                Console.WriteLine("Informe o modelo do veículo");
+                NomeModelo = Console.ReadLine();
+                Console.WriteLine("Informe a quantidade de quilômetros rodados");
+                quilometro = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Informe a potência do motor");
+                potencia = Convert.ToDouble(Console.ReadLine());
+
+                if (quilometro <= 5000)
+                    ClassifQuil = "Novo";
+                else if (quilometro > 5000 && quilometro <= 30000)
+                    ClassifQuil = "Seminovo";
+                else
+                    ClassifQuil = "Velho";
+
+                if (potencia > 200)
+                    ClassifPot = "Potente";
+                else if (potencia == 120 && potencia <= 200)
+                    ClassifPot = "Forte";
+                else
+                    ClassifPot = "Popular";
+
+                Console.WriteLine("{0} - {1} - {2}", NomeModelo, ClassifQuil, ClassifPot);
+            }
 
         }
     }
