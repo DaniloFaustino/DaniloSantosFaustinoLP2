a2a092663a5b34cdf0f0e0cd052956c80e6ae90b
DaniloFaustino <danilovid@yahoo.com.br>
2017-05-11 20:48:06 -0300

    Revisão Final

--- Soluções/Revisao/Revisao.cs
+++ Soluções/Revisao/Revisao.cs
@@ -17,13 +17,18 @@ namespace ExercicioRevisao
 
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
+            int n;
+            Console.WriteLine("Informe a quantidade de carros"); 
+            n = int.Parse(Console.ReadLine());
             Carro[] carros = new Carro[n];
 
             for (int i = 0; i < n; i++)
             {
+                Console.WriteLine("Informe o modelo do carro {0}", i + 1);
                 carros[i].Modelo = Console.ReadLine();
-                carros[i].Quilometragem = double.Parse(Console.ReadLine()); 
+                Console.WriteLine("Informe a quilometragem do carro {0}", i + 1);
+                carros[i].Quilometragem = double.Parse(Console.ReadLine());
+                Console.WriteLine("Informe a potência do carro {0}", i + 1);
                 carros[i].Potencia = int.Parse(Console.ReadLine());
             }
 
