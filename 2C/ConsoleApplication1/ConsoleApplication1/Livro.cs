0e7ee8d3cba29715032ee344f0149853e15f6ec4
DaniloFaustino <danilovid@yahoo.com.br>
2017-07-04 15:56:36 -0300

    Hermione

---
+++ 2C/ConsoleApplication1/ConsoleApplication1/Livro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Livro
+    {
+        public  string titulo;
+        public double paginas, dias; 
+        public double TempoLeituraHoras(double pph)
+        {
+            double total = paginas / pph; 
+            return total;
+        }
+    }
+}
---
+++ 2C/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pph, hpd, total;
+            int paginas, dias, n; 
+            string titulo;
+
+            Console.WriteLine("Informe a quantidade de livros");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a quantidade de páginas por hora");
+            pph = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe a quantidade de horas diárias");
+            hpd = Convert.ToDouble(Console.ReadLine());
+
+
+            Livro[] livros = new Livro[n];
+ 
+            for (int i = 0; i < n; i++)
+            {
+                Livro livro = new Livro();
+
+            }
+        }
+    }
+}
