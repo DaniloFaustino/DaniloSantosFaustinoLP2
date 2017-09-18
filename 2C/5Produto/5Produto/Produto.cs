e3b181d3e1ae0699f318d9cdd8e501d40601a96e
Arthur <cogupl4ys@gmail.com>
2017-09-18 13:37:30 -0300

    Produto

--- 2C/5Produto/5Produto/Produto.cs
+++ 2C/5Produto/5Produto/Produto.cs
@@ -9,30 +9,37 @@ namespace _5Produto
     class Produto
     {
         private int id;
-
         public int Id
         {
             get { return id; }
         }
-        public string Nome { get; set; }
+
+        private string nome; 
+        public string Nome {
+            get { return nome; }
+        }
 
         private int quatidade;
-
         public int Quantidade
         {
             get { return quatidade; }
         }
+
         public double Preco { get; set; }
 
         public void Repor(int repor)
         {
             this.quatidade += repor;
         }
+        public void Retirar(int retirar)
+        {
+            this.quatidade -= retirar; 
+        }
 
-        public Produto(int id, string nome, int preco)
+        public Produto(int id, string nome, double preco)
         {
             this.id = id;
-            this.Nome = nome;
+            this.nome = nome;
             this.Preco = preco;
             this.quatidade = 0;
         }
