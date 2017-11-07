2026d2a4fbc7d55f399acdc788e0eca692a7a45a
Danilo <danilovid@yahoo.com.br>
2017-11-07 15:48:49 -0300

    Tentativas

---
+++ Rascunhos/PrimeiroBank/PrimeiroBank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection conex = new MySqlConnection("Server=localhost; Database=organizador; Uid=root; Pwd=");
+            MySqlCommand comd = new MySqlCommand();
+
+            comd.Connection = conex;
+            comd.CommandText = "INSERT INTO (Id, Nome) VALUE (1, 'Dynasty');";
+        }
+    }
+}
